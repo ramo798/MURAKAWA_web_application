7d6438a6d34193cbbe20d9e2a9d455a08df12ffe
ramo <ramo@ramo.local>
2019-12-13 12:21:06 +0900

    静的なhtmlの配信まで。クエリパラメータの受け取りテスト完了

--- cgi/go/main.go
+++ cgi/go/main.go
@@ -5,11 +5,30 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World")
+func receiver(w http.ResponseWriter, r *http.Request){
+	// クエリパラメータ取得してみる
+	fmt.Fprintf(w, "クエリ：%s\n", r.URL.RawQuery)
+
+	// Bodyデータを扱う場合には、事前にパースを行う
+	r.ParseForm()
+
+	// Formデータを取得.
+	form := r.PostForm
+	fmt.Fprintf(w, "フォーム：\n%v\n", form)
+	fmt.Fprintf(w, "フォーム：\n%v\n", form["password"])
+
+
+
+	// または、クエリパラメータも含めて全部.
+	params := r.Form
+	fmt.Fprintf(w, "フォーム2：\n%v\n", params)
 }
 
+
+
 func main() {
-	http.HandleFunc("/", handler)
+	http.Handle("/get/", http.StripPrefix("/get/", http.FileServer(http.Dir("./html/get/"))))
+	http.Handle("/post/", http.StripPrefix("/post/", http.FileServer(http.Dir("./html/post/"))))
+	http.HandleFunc("/receive", receiver)
 	http.ListenAndServe(":8080", nil)
 }
